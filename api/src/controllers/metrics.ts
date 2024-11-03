726b7cb4a9e50c8d8f88bbf094020f794ae7c387
llortegall <ivanortega_97@hotmail.com>
2024-11-03 13:06:15 -0500

    refactor: add new general metrics controller

--- api/src/controllers/metrics.ts
+++ api/src/controllers/metrics.ts
@@ -1,1 +1,60 @@
+import { Response, Request } from 'express'
 import { Count } from '../models/count.m.js'
+import { col, fn, Op } from 'sequelize';
+
+export const getGeneralMetrics = async (req: Request, res: Response) => {
+  const yearActual = new Date().getFullYear();
+  const monthActual = new Date().getMonth();
+
+  const firtsDay = new Date(yearActual, monthActual, 1).toISOString().split('T')[0]!;
+  const lastDay = new Date(yearActual, monthActual + 1, 0).toISOString().split('T')[0]!;
+
+  const firtsDayMonthAnt = new Date(yearActual, monthActual - 1, 1).toISOString().split('T')[0]!;
+  const lastDayMonthAnt = new Date(yearActual, monthActual, 0).toISOString().split('T')[0]!;
+
+  try {
+    const metricsMonthActual = await Count.findAll({
+      attributes: [
+        [fn('SUM', col('countPlay')), 'totalPlays'],
+        [fn('SUM', col('countDownload')), 'totalDownloads'],
+        [fn('SUM', col('countProcess')), 'totaProcess'],
+        [fn('COUNT', fn('DISTINCT', col('username'))), 'uniqueUsers']
+      ],
+      where: {
+        dateSave: {
+          [Op.between]: [firtsDay, lastDay]
+        }
+      }
+    })
+
+    const metricsMonthAnt = await Count.findAll({
+      attributes: [
+        [fn('SUM', col('countPlay')), 'totalPlays'],
+        [fn('SUM', col('countDownload')), 'totalDownloads'],
+        [fn('SUM', col('countProcess')), 'totaProcess'],
+        [fn('COUNT', fn('DISTINCT', col('username'))), 'uniqueUsers']
+      ],
+      where: {
+        dateSave: {
+          [Op.between]: [firtsDayMonthAnt, lastDayMonthAnt]
+        }
+      }
+    })
+
+    const sendData = {
+      metricsMonthAnt : {
+        counts: metricsMonthAnt[0],
+        rango: [firtsDayMonthAnt, lastDayMonthAnt]
+      },
+      metricsMonthAct: {
+        counts: metricsMonthActual[0],
+        rango: [firtsDay, lastDay]
+      }
+    }
+
+    res.status(200).json(sendData)
+  } catch (error) {
+    res.status(500).json('fail')
+  }
+
+}
