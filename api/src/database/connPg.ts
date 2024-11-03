94f63b96109d35898c63e2add157f4d91fec381c
llortegall <ivanortega_97@hotmail.com>
2024-11-03 13:06:34 -0500

    refactor: show logs depends on enviroment value

--- api/src/database/connPg.ts
+++ api/src/database/connPg.ts
@@ -1,9 +1,9 @@
 import { Sequelize } from "sequelize";
-import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER, DB_PORT } from '../schemas/env.js'
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER, DB_PORT, ENV } from '../schemas/env.js'
 
 export const connPostgreSQL = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   port: DB_PORT,
   dialect: 'postgres',
-  logging: false,
+  logging: ENV !== 'dev' ? false : true
 });
