533a63c1b96121d48d1a80762bd52276c583c15e
llortegall <ivanortega_97@hotmail.com>
2024-11-03 13:07:07 -0500

    refactor: firts render metrics component info

--- client/src/pages/Metrics.tsx
+++ client/src/pages/Metrics.tsx
@@ -1,10 +1,57 @@
-import { Card } from "@/components/ui/card";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import axios from "axios";
+import { ChartColumn } from "lucide-react";
+import { useEffect, useState } from "react";
+
+interface CountInt {
+  totalPlays: string,
+  totalDownloads: string,
+  totaProcess: string
+  uniqueUser: string
+}
+
+interface DataGeneralMetrics {
+  metricsMonthAnt: {
+    counts: CountInt[];
+    rango: string[];
+  };
+  metricsMonthAct: {
+    counts: CountInt;
+    rango: string[];
+  };
+}
 
 export const MetricsComponent = () => {
+  const [data, setData] = useState<DataGeneralMetrics | undefined>(undefined)
+
+  useEffect(() => {
+    axios.get('/metrics/general')
+      .then(res => setData(res.data))
+      .catch(err => console.log(err))
+  }, [])
 
   return (
-    <Card>
-      New Metrics
-    </Card>
+    <section className="p-8">
+      <CardHeader className="pt-6">
+        <section className="flex items-center gap-1">
+          <ChartColumn />
+          <CardTitle>
+            Resumen Global Mensual
+          </CardTitle>
+        </section>
+        <p className="text-gray-400">
+          Consolidado Mensual de uso general mes actual y anterior
+        </p>
+
+      </CardHeader>
+
+      <Card>
+        <CardTitle>
+          {
+
+          }
+        </CardTitle>
+      </Card>
+    </section>
   );
 };
