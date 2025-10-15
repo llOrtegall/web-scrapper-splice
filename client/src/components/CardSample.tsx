dc959fbcd48852b78babef7cc6433f380e586587
llortegall <ivanortega_97@hotmail.com>
2025-10-15 13:21:11 -0500

    modify component to use shadcn

--- client/src/components/CardSample.tsx
+++ client/src/components/CardSample.tsx
@@ -1,8 +1,9 @@
 import { AudioLines, Download, PlayCircle, StopCircle } from "lucide-react";
-import type { Item } from "../types/searhResponse";
 import { useEffect, useRef, useState } from "react";
-import axios from "axios";
 import { decodeSpliceAudio } from "@/utils/decoder";
+import type { Item } from "../types/searhResponse";
+import axios from "axios";
+import { Card, CardContent } from "./ui/card";
 
 export function CardSample({ items }: { items: Item[] }) {
   const [playingId, setPlayingId] = useState<string | null>(null);
@@ -129,25 +130,23 @@ export function CardSample({ items }: { items: Item[] }) {
   return (
     <ul className="space-y-2">
       {items.map(sample => (
-        <li key={sample.uuid} className="grid p-3 rounded bg-gray-800 border border-gray-700 grid-cols-1 sm:grid-cols-4 md:grid-cols-8 lg:grid-cols-12 gap-2 md:gap-4 items-center">
+        <Card key={sample.uuid}>
 
-          <figure className="flex items-center justify-around gap-2 col-span-1 sm:col-span-1 md:col-span-2 lg:col-span-2">
-            <img src={extractImageUrl(sample)} alt={sample.name} className="rounded-sm" width={68} height={68} loading="lazy" />
-
-            <button className="p-1 rounded bg-gray-700">
-              <AudioLines className="w-8 h-8 text-blue-200" />
-            </button>
+          <CardContent className="flex items-center gap-2">
+            <figure>
+              <img src={extractImageUrl(sample)} alt={sample.name} className="rounded-sm" width={68} height={68} loading="lazy" />
+            </figure>
 
             <button
               onClick={() => handlePlayClick(sample)}
-              className={`p-1 rounded bg-gray-700 hover:bg-gray-600 cursor-pointer flex items-center justify-center relative`}
+              className={`bg-gray-800 p-2 rounded-md hover:bg-gray-700 cursor-pointer flex items-center justify-center relative`}
               disabled={audioLoading && playingId === sample.uuid}
               title={playingId === sample.uuid ? "Stop" : "Play"}
             >
               {playingId === sample.uuid ? (
-                <StopCircle className="w-8 h-8 text-red-400" />
+                <StopCircle className="text-red-500" />
               ) : (
-                <PlayCircle className="w-8 h-8 text-yellow-300" />
+                <PlayCircle className="text-green-600" />
               )}
               {audioLoading && playingId === sample.uuid && (
                 <span className="absolute right-0 top-0 text-xs text-blue-300 animate-pulse">Loading...</span>
@@ -156,49 +155,25 @@ export function CardSample({ items }: { items: Item[] }) {
 
             <button
               onClick={() => handleDownload(sample)}
-              className="p-1 rounded bg-gray-700 hover:bg-gray-600 cursor-pointer"
+              className="bg-gray-800 p-2 rounded-md hover:bg-gray-700 cursor-pointer flex items-center"
               title="Download sample"
             >
-              <Download className="w-8 h-8 text-green-300" />
+              <Download className="text-gray-300" />
             </button>
 
-          </figure>
+          </CardContent>
 
-          <section className="flex flex-col justify-center col-span-1 sm:col-span-2 md:col-span-4 lg:col-span-4">
-            <div
-              className="font-bold text-blue-300 truncate max-w-[180px] md:max-w-[320px] lg:max-w-[480px]"
-              title={sample.name.split("/").pop()}
-            >
+          <CardContent className="flex items-center gap-2">
+            <div title={sample.name.split("/").pop()} >
               {sample.name.split("/").pop()}
             </div>
-            <div className="flex items-center space-x-2">
+            <div>
               <span>BPM: {sample.bpm ?? "---"}</span>
-              <span className="ml-4">Type: {sample.asset_category_slug ?? "-"}</span>
+              <span>Type: {sample.asset_category_slug ?? "-"}</span>
             </div>
-          </section>
-
-          <div className="flex items-center col-span-1 sm:col-span-1 md:col-span-2 lg:col-span-2">
-            <canvas
-              width={250}
-              height={30}
-              className="bg-gray-700 rounded shadow-inner border border-gray-600"
-              style={{
-                display: "block",
-                boxSizing: "border-box",
-                outline: "none",
-                transition: "box-shadow 0.2s",
-              }}
-            ></canvas>
-          </div>
-
-          <section className="flex flex-col justify-center ml-auto text-right col-span-1">
             <div>Time: {formatDuration(sample.duration)}</div>
-          </section>
-
-          <section className="flex flex-col justify-center ml-auto text-right col-span-1">
-            <div>Key: {sample.key ?? "-"}</div>
-          </section>
-        </li>
+          </CardContent>
+        </Card>
       ))}
     </ul>
   )
