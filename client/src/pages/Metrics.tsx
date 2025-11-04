6f65eecf652b2f6631951b7ddb416de656e90f25
llortegall <ivanortega_97@hotmail.com>
2025-11-04 15:09:14 -0500

    refactor: modify MetricsComponente and new card metrics adds

--- client/src/pages/Metrics.tsx
+++ client/src/pages/Metrics.tsx
@@ -1,18 +1,19 @@
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { ChartColumn } from "lucide-react";
-import { useEffect, useState } from "react";
+import { CardMontMetric } from "@/components/CardMonthMetric";
 
 interface CountInt {
-  totalPlays: string,
-  totalDownloads: string,
-  totaProcess: string
-  uniqueUser: string
+  totalPlays: number;
+  totalDownloads: number;
+  totalProcess: number;
+  uniqueUsers: number;
 }
 
 interface DataGeneralMetrics {
   metricsMonthAnt: {
-    counts: CountInt[];
+    counts: CountInt;
     rango: string[];
   };
   metricsMonthAct: {
@@ -21,23 +22,29 @@ interface DataGeneralMetrics {
   };
 }
 
+
+
 export const MetricsComponent = () => {
   const [data, setData] = useState<DataGeneralMetrics | undefined>(undefined)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
+    setLoading(true)
     axios.get('/metrics/general')
       .then(res => setData(res.data))
-      .catch(err => console.log(err))
+      .catch(err => console.error('metrics fetch error', err))
+      .finally(() => setLoading(false))
   }, [])
 
+
   return (
-    <section className="p-8">
+    <Card>
       <CardHeader className="pt-6">
         <section className="flex items-center gap-1">
           <ChartColumn />
-          <CardTitle>
-            Resumen Global Mensual
-          </CardTitle>
+          <div>
+            <CardTitle className="text-lg md:text-xl">Resumen Global Mensual</CardTitle>
+          </div>
         </section>
         <p className="text-gray-400">
           Consolidado Mensual de uso general mes actual y anterior
@@ -45,13 +52,20 @@ export const MetricsComponent = () => {
 
       </CardHeader>
 
-      <Card>
-        <CardTitle>
-          {
+      <CardMontMetric
+        dateStart={data?.metricsMonthAct.rango[0]!}
+        dateFinal={data?.metricsMonthAct.rango[1]!}
+        counts={data?.metricsMonthAct.counts!}
+        loading={loading}
+      />
 
-          }
-        </CardTitle>
-      </Card>
-    </section>
+      <CardMontMetric
+        dateStart={data?.metricsMonthAnt.rango[0]!}
+        dateFinal={data?.metricsMonthAnt.rango[1]!}
+        counts={data?.metricsMonthAnt.counts!}
+        loading={loading}
+      />
+
+    </Card>
   );
 };
