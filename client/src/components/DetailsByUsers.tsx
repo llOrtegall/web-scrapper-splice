50dc08e516e20968fa9ae697f6686156aad0656d
llortegall <ivanortega_97@hotmail.com>
2025-11-05 15:33:25 -0500

    Add: new component to details user usage

---
+++ client/src/components/DetailsByUsers.tsx
@@ -0,0 +1,41 @@
+import { Button } from "@/components/ui/button"
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
+
+
+export function DetailsByUsers() {
+
+  return (
+    <Drawer>
+      <DrawerTrigger asChild>
+
+        <Button variant="outline">Ver Detallado Usuarios</Button>
+      </DrawerTrigger>
+
+      <DrawerContent>
+        <div className="mx-auto w-full max-w-sm">
+          <DrawerHeader>
+            <DrawerTitle>Move Goal</DrawerTitle>
+            <DrawerDescription>Set your daily activity goal.</DrawerDescription>
+          </DrawerHeader>
+          <div className="p-4 pb-0">
+
+          </div>
+          <DrawerFooter>
+            <DrawerClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  )
+}
