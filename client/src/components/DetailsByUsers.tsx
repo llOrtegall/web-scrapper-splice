4fbfb8230c9c7b28d9541a2f9964861a363550c9
llortegall <ivanortega_97@hotmail.com>
2025-11-06 16:11:38 -0500

    refactor: modify view details users

--- client/src/components/DetailsByUsers.tsx
+++ client/src/components/DetailsByUsers.tsx
@@ -10,32 +10,89 @@ import {
   DrawerTrigger,
 } from "@/components/ui/drawer"
 
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
-export function DetailsByUsers() {
+import axios from "axios"
+import { useState } from "react"
+
+interface Props {
+  initialDate: string,
+  finalDate: string
+}
+
+interface DataUsageUser {
+  username: string,
+  totalPlays: string
+  totalDownloads: string
+  totalProcess: string
+}
+
+export function DetailsByUsers({ finalDate, initialDate }: Props) {
+  const [dataUsage, setDataUsage] = useState<DataUsageUser[] | null>(null)
+
+  const handleOpenDetails = () => {
+    axios.post('/metrics/monthUsers', { firtsDate: initialDate, finalDate })
+      .then(res => {
+        setDataUsage(res.data)
+      })
+      .catch(err => console.log(err))
+  }
 
   return (
     <Drawer>
       <DrawerTrigger asChild>
-
-        <Button variant="outline">Ver Detallado Usuarios</Button>
+        <Button variant="outline" onClick={handleOpenDetails}>Ver Detallado Usuarios</Button>
       </DrawerTrigger>
 
       <DrawerContent>
-        <div className="mx-auto w-full max-w-sm">
+        <div className="px-64">
           <DrawerHeader>
-            <DrawerTitle>Move Goal</DrawerTitle>
-            <DrawerDescription>Set your daily activity goal.</DrawerDescription>
+            <DrawerTitle>Detallado Usuario</DrawerTitle>
+            <DrawerDescription>En esta sección se mostrará el uso de los diferentes servicios prestados por splice downloader con base al mes seleccionado anteriormente</DrawerDescription>
           </DrawerHeader>
-          <div className="p-4 pb-0">
 
-          </div>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="">Nombre Usuario</TableHead>
+                <TableHead>Reproducciones</TableHead>
+                <TableHead>Descargas</TableHead>
+                <TableHead>Procesados</TableHead>
+                <TableHead className="text-right">Total</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {
+                dataUsage?.map(d => (
+                  <TableRow>
+                    <TableCell className="font-medium">{d.username}</TableCell>
+                    <TableCell>{d.totalPlays}</TableCell>
+                    <TableCell>{d.totalDownloads}</TableCell>
+                    <TableCell>{d.totalProcess}</TableCell>
+                    <TableCell className="text-right">
+                      {parseInt(d.totalPlays) + parseInt(d.totalDownloads) + parseInt(d.totalProcess)}
+                    </TableCell>
+                  </TableRow>
+                ))
+              }
+            </TableBody>
+          </Table>
+
+
           <DrawerFooter>
             <DrawerClose asChild>
-              <Button variant="outline">Cancel</Button>
+              <Button variant="outline" className="w-40">Cerrar</Button>
             </DrawerClose>
           </DrawerFooter>
         </div>
       </DrawerContent>
-    </Drawer>
+    </Drawer >
   )
 }
